2562cb0ebd70e2756b9b74fa80983d2b1dc54b9e
jneira <atreyu.bbb@gmail.com>
2013-02-12 15:46:06 +0100

    forgotten files

--- basicos.pl
+++ basicos.pl
@@ -41,7 +41,7 @@ divide(X,Y,Z):-
 
 %% Ejercicio 3.3
 
-substituye(X,Y,[],[]).
+substituye(_,_,[],[]).
 substituye(X,Y,[X|TX],[X|TY]):-substituye(X,Y,TX,TY).
 substituye(X,Y,[_|TX],[_|TY]):-substituye(X,Y,TX,TY).
 
@@ -55,3 +55,59 @@ permutacion(1,[X|TX],[Y|TY],[Y|TX1],[X|TY1]):-
 permutacion_par(XS,YS,S1,S2):-permutacion(0,XS,YS,S1,S2).
 permutacion_impar(XS,YS,S1,S2):-permutacion(1,XS,YS,S1,S2).
 
+%% sort list
+
+bubble([],[]).
+bubble([X],[X]).
+bubble([X1,X2|TX],[X1|TY]):-X2>=X1,bubble([X2|TX],TY).
+bubble([X1,X2|TX],[X2|TY]):-bubble([X1|TX],TY).
+
+my_sort(XS,XS):-bubble(XS,XS).
+my_sort(XS,YS):-bubble(XS,ZS),my_sort(ZS,YS).
+
+%% plagelao version
+
+weird_sort([], []).
+weird_sort([Element], [Element]).
+weird_sort([First,Second|Tail], [First,Second|SortedTail]) :-
+        First =< Second,
+        weird_sort(Tail, SortedTail).
+weird_sort([Head|Tail], [SortedHead|NewSortedTail]) :-
+        weird_sort(Tail, [SortedHead|SortedTail]),
+        weird_sort([Head|SortedTail], NewSortedTail).
+
+
+nat(0).
+nat(succ(X)):-nat(X).
+
+add(0,Y,Y).
+add(succ(X),Y,succ(Z)) :-
+        add(X,Y,Z).
+
+greater_than(succ(X),0).
+greater_than(succ(X),succ(Y)):-greater_than(X,Y).
+
+% Exercise 10.1
+p(1).
+p(2):-!.
+p(3).
+
+% p(X). X=1
+% p(X),p(Y). X=1 Y=1
+% p(X),!,p(Y). X=1 Y=1
+
+% Exercise 10.2
+
+class(Number,positive) :- Number > 0,!.
+class(0,zero).
+class(Number, negative) :- Number < 0,!.
+
+% Exercise 10.3
+% split([3,4,-5,-1,0,4,-9],P,N)
+
+split([],[],[]).
+split([H|T],[H|TX],Y):-
+        not(class(H,negative)),
+        split(T,TX,Y),!.
+split([H|T],X,[H|TY]):-
+        split(T,X,TY),!.
--- examen.pl
+++ examen.pl
@@ -1,3 +1,5 @@
+%% Febrero 2011-2
+
 objeto([a,b]).
 bloque([[a],[b]]).
 
@@ -22,3 +24,93 @@ mover_bloque([X,Y],[[Y]|XS],[[X,Y]|YS]):-
         mover_bloque([X,Y],XS,YS).
 mover_bloque([X,Y],[Z|XS],[Z|YS]):-
         mover_bloque([X,Y],XS,YS).
+
+%% Septiembre 2011-1
+
+%%Codigo con errores
+% repetir:-repetir.
+% repetir.
+
+repetir.
+% repetir:-repetir.
+
+nombre(pedro).
+nombre(ana).
+nombre(juan).
+nombre(lola).
+
+% nombres:-repetir,nombre(X),write(X),nl,fail.
+nombres:-repetir,nombre(X),write(X),nl,fail.
+%% El predicado repetir es un loop recursivo infinito ya que
+%% la condicion de de terminacion esta despues de la que establece
+%% la llamada recursiva.
+%% El predicado nombres tambien entra en un loop infinito ya que
+%% el ultimo fail provoca que el bactracking vuelva al primer predicado,
+%% intentando la llamada recursiva y volviendo a imprimir los 4 nombres
+
+%% Problema 4
+
+%% buscar(3,[[1,0,3],[1,2,-1]],3,1).
+%% yes.
+%% buscar(1,[[1,0,3],[1,2,-1]],X,Y).
+%% X=1,Y=1
+%% X=1,Y=2
+%% buscar(X,[[1,0,3],[1,2,-1]],3,2).
+%% X=-1
+
+buscar(X,[[X|_]|_],1,1).
+buscar(E,[[_|XS]|S],I,1):-
+        buscar(E,[XS|S],I2,1),I is I2+1.
+buscar(E,[_|XS],I,J):-
+        buscar(E,XS,I,J2),J is J2+1.
+
+%% Septiembre 2010-1
+
+%% Problema 3
+%% triangulo_siguiente_fila([1,2,1],X).
+%% X=[1,3,3,1]
+%% triangulo_siguiente_fila([1,3,3,1],X).
+%% X=[1,4,6,4,1]
+pascal_next([X|T],[X|TR]):-
+        pascal_next_tail([X|T],TR).
+pascal_next_tail([X],[X]).
+pascal_next_tail([X,Y|T],[XY|TR]):-
+        pascal_next_tail([Y|T],TR),XY is X+Y.
+
+%% Problema 4
+%% triangulo(0,Fila).
+%% Fila=[1]
+%% triangulo(3,Fila).
+%% Fila=[1,4,6,4,1]
+
+pascal_row(0,[1]).
+pascal_row(1,[1,2,1]).
+pascal_row(X,Row):-
+        Y is X-1,
+        pascal_row(Y,NextRow),
+        pascal_next(NextRow,Row).
+        
+%% Septieembre 2010-2
+
+%% numeros(2,7,[2,3,4,5,6,7])
+
+numeros(X,X,[X]).
+numeros(X,Y,[X|XS]):-
+        X<Y,XN is X+1,numeros(XN,Y,XS).
+
+% numeros(2,7,[2|[3,4,5,6,7])
+% 2<7,XN is 3
+% numeros(3,7,[3|[4,5,6,7])
+% 3<7,XN is 4
+% numeros(4,7,[4,5,6,7])
+% 4<7, XN is 5
+% numeros(5,7,[5,6,7])
+% 4<7, XN is 6
+% numeros(4,7,[4,5,6,7])
+% 4<7, XN is 5
+
+%% Problema 4
+
+aplanar([],[]).
+aplanar([[X]|XS],[X|RS]):-aplanar(XS,RS).
+
